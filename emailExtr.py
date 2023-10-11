46d6326950c39acbe50f6d7558301d49acfa23d7
Szulyka <palko@horogszegi.hu>
2023-10-11 10:59:09 +0200

    datasafety added

--- emailExtr.py
+++ emailExtr.py
@@ -3,16 +3,17 @@ import email
 import time
 import pyperclip 
 
-#this program connect to your email account, gets the content latest Neptun email every 4 seconds and copies the security 6-digit code to the clipboard
+#this program connect to your e-mail account, gets the content latest Neptun e-mail every 4 seconds and copies the security 6-digit code to the clipboard
 
 while True:
+    #could be other e-mail client
     imapServer = imaplib.IMAP4_SSL('imap.gmail.com')
 
     #your e-mail adress, and password
     #for the password, you need to make an app password through google -> profile settings -> security -> app passwords 
     #this is needed to surpass two-step authentication at Google
     #do not share that code with anyone, it is a security risk
-    imapServer.login('horogszegi.palko@gmail.com', 'bzdm jufm eehs ltda')
+    imapServer.login('e-mail', 'password')
     #example: -------'smith.adam@gmail.com', '4 times 4 digit app password'
 
     imapServer.select('inbox')
@@ -20,17 +21,17 @@ while True:
     result, emailIds = imapServer.search(None, 'FROM "noreply@neptun.elte.hu" SUBJECT "Neptun"')
 
     if result == 'OK':
-        # Get the latest email ID
+        # Get the latest e-mail ID
         latestEmailId = emailIds[0].split()[-1]
         print(latestEmailId)
-        # Fetch the email content
+        # Fetch the e-mail content
         result, emailData = imapServer.fetch(latestEmailId, '(RFC822)')
         
-        # Parse the email content
+        # Parse the e-mail content
         rawEmail = emailData[0][1]
         emailMessage = email.message_from_bytes(rawEmail)
         
-        # Extract the text from the email body
+        # Extract the text from the e-mail body
         emailText = emailMessage.get_payload()
      
         #ascii -> utf-8 causes some corruption to the text
@@ -55,7 +56,9 @@ while True:
     else:
         print("No email found")
         
-    #logout in every iteration, probably there is a better way. This ensures that you will get the latest email every iteration
+    #logout in every iteration, probably there is a better way. This ensures that you will get the latest e-mail every iteration
     imapServer.logout()
             
-    time.sleep(4)
+    time.sleep(1)
+
+#TODO: standalone app that calls this app and automatically click e-mail button, pastes the code, pushes enter
