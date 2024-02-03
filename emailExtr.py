2e0c90c6ad1b42c403e52281aa5fa8ebe05da522
Szulyka <palko@horogszegi.hu>
2024-02-03 15:06:59 +0100

    logic perfect

--- emailExtr.py
+++ emailExtr.py
@@ -1,75 +1,80 @@
-import imaplib
-import email
+import subprocess
+import sys
 import time
-import pyperclip 
+import quopri
+import email
 
-#this program connect to your e-mail account, gets the content latest Neptun e-mail every 4 seconds and copies the security 6-digit code to the clipboard
+
+def install_dependencies():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing dependencies: {e}")
+        sys.exit(1)
+
+
+# Install dependencies if not already installed
+try:
+    import imaplib
+    import pyperclip
+except ImportError:
+    print("Missing dependencies, installing now...")
+    install_dependencies()
+
+# this program connect to your e-mail account, gets the content of latest Neptun e-mail every 4 seconds and copies the security 6-digit code to the clipboard
+
+# could be other e-mail client
+imapServer = imaplib.IMAP4_SSL('imap.gmail.com')
+
+# your e-mail adress, and password
+# for the password, you need to make an app password through Google -> profile settings -> security -> app passwords
+# this is needed to surpass two-step authentication at Google
+# do not share that code with anyone, it is a security risk
+imapServer.login('horogszegi.palko@gmail.com', 'bzdm jufm eehs ltda')
+# example: -------'smith.adam@gmail.com', '4 times 4 digit app password'
 
 latestEmailId = ""
-
 while True:
-    #could be other e-mail client
-    imapServer = imaplib.IMAP4_SSL('imap.gmail.com')
-
-    #your e-mail adress, and password
-    #for the password, you need to make an app password through google -> profile settings -> security -> app passwords 
-    #this is needed to surpass two-step authentication at Google
-    #do not share that code with anyone, it is a security risk
-    imapServer.login('email', 'pswd')
-    #example: -------'smith.adam@gmail.com', '4 times 4 digit app password'
-
     imapServer.select('inbox')
-    #example: ---------------------------------------"noreply@neptun.elte.hu"         "Neptun"       <- don't add utf-8 chars, also subset of words of the subject is enough
+    # example: ------------------------------------------------------"noreply@neptun.elte.hu"        "Neptun"       <- don't add utf-8 chars, also subset of words of the subject is enough
     result, emailIds = imapServer.search(None, 'FROM "noreply@neptun.elte.hu" SUBJECT "Neptun"')
- 
-        
     if result == 'OK':
-        #this lets the code and the clipboard "rest" until a new Neptun e-mail comes
-        if emailIds[0].split()[-1] != latestEmailId: 
+        # this lets the code and the clipboard "rest" until a new Neptun e-mail comes
+        if emailIds[0].split()[-1] != latestEmailId:
             # Get the latest e-mail ID
             latestEmailId = emailIds[0].split()[-1]
-            print(latestEmailId)
-            print(emailIds[0].split()[-1])
-            
-                
+
             # Fetch the e-mail content
             result, emailData = imapServer.fetch(latestEmailId, '(RFC822)')
-            
+
             # Parse the e-mail content
             rawEmail = emailData[0][1]
+
             emailMessage = email.message_from_bytes(rawEmail)
-            
             # Extract the text from the e-mail body
             emailText = emailMessage.get_payload()
-        
-            #ascii -> utf-8 causes some corruption to the text
-            emailText = emailText.replace('=', '')
-            
-            numList = []
-            for i in range(len(emailText)):
-                #not sure why this is how it works 
-                if i > 30 and emailText[i].isdigit() and len(numList) < 22:
-                    numList.append(emailText[i])
-                    
-            #numList is the whole code, Neptun already includes the first three digits, so codeWithoutFirstThree is what we need
-            numList = numList[13:]
-            codeWithoutFirstThree = numList[3:]
-            codeWithoutFirstThree = ('').join(codeWithoutFirstThree)
-            #let's put it onto clipboard
-            pyperclip.copy(codeWithoutFirstThree)
-            print("Code to be pasted: ", codeWithoutFirstThree)
-            
-            #whole e-mail
-            print(emailText)
+            decodedEmailText = quopri.decodestring(emailText).decode('utf-8')
+            # whole e-mail
+            print("EMAIL CONTENT")
+            print(decodedEmailText)
+
+            digitsOfCode = []
+            # we go through char by char in the email body, after finding digits, we ignore the first three digits and dash and there is the 6-digit code we need
+            for i in range(len(decodedEmailText)):
+                if decodedEmailText[i].isdigit():
+                    digitsOfCode.append(decodedEmailText[i + 4:i + 10])
+                    break
+
+            digitsOfCode = ''.join(digitsOfCode)
+            print(digitsOfCode)
+            # let's put it onto clipboard
+            pyperclip.copy(digitsOfCode)
+            print("Code to be pasted: ", digitsOfCode)
         else:
-            print("waiting for new code")
+            print("Waiting for new code")
+            time.sleep(1)
     else:
         print("No email found")
-            
-        #logout in every iteration, probably there is a better way. This ensures that you will get the latest e-mail every iteration
-        imapServer.logout()
-                
-        time.sleep(1)
 
- 
-#TODO: standalone app that calls this app and automatically click e-mail button, pastes the code, pushes enter
+
+# TODO: standalone app that calls this app and automatically click e-mail button, pastes the code, pushes enter
